b3246aefe58d1f537618c5643fbf794aa7ecddbb
Zuhair <zuhair.ghias@hotmail.com>
2022-10-01 17:25:07 -0500

    Fix ItemData

--- Source/Assets/Scripts/Objects/Item.cs
+++ Source/Assets/Scripts/Objects/Item.cs
@@ -1,7 +1,16 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 public abstract class Item : MonoBehaviour
 {
+    public Action<string> StatusChanged;
 
+    /// <summary>
+    /// HUD can use this to get description/status/ammo count etc.
+    /// </summary>
+    public string Status {
+        get { return Status; }
+        set { StatusChanged.Invoke(value); Status = value; }
+    }
     public abstract void Use();
 }
