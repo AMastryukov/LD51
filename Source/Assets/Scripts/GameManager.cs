989df897d7c251224c823762555d3168056c7b67
QuantumCalzone <QuantumCalzone@gmail.com>
2022-10-01 12:03:57 -0700

    Queues managed and hooked up to Game Manager

--- Source/Assets/Scripts/GameManager.cs
+++ Source/Assets/Scripts/GameManager.cs
@@ -12,6 +12,8 @@ public enum GameStates
 
 public class GameManager : MonoBehaviour
 {
+    private const int SECONDS_TO_COUNT_TO = 10;// change this if 10 seconds is too long for your attention span
+
     public static GameManager Instance { get; private set; }
 
     public Action<GameStates> OnGameStateChanged;
@@ -24,13 +26,64 @@ public class GameManager : MonoBehaviour
     public Action OnTimerStarted;
     public Action OnTimerStopped;
 
+    public Action<string> OnNewWeapon;
+    public Action<string> OnNewTrap;
+    public Action<Bonuses> OnNewBonus;
+
+    [Tooltip("The max count of each queue when populating; inclusive.")]
+    [SerializeField] private int queueMax = 5;
+
+    [Tooltip("When a queue reaches this amount, it will generate up to the count of queueMax")]
+    [SerializeField] private int repopulateQueueAtCount = 2;
+
     [SerializeField] private bool verboseLogging = false;
     [SerializeField] private bool superVerboseLogging = false;
 
+    private Queue<string> weaponQueue = new Queue<string>();
+    private Queue<string> trapQueue = new Queue<string>();
+    private Queue<Bonuses> bonusQueue = new Queue<Bonuses>();
+
+    private string lastQueuedWeapon = null;
+    private string lastQueuedTrap = null;
+    private Bonuses lastQueuedBonus = Bonuses.None;
+
     private GameStates gameState = GameStates.Menu;
     private IEnumerator gameTimer = null;
+    private int bonusesLength = 0;
 
     public GameStates GameState => gameState;
+    public Queue<string> WeaponQueue => weaponQueue;//todo make readonly
+    public Queue<string> TrapQueue => trapQueue;//todo make readonly
+    public Queue<Bonuses> BonusQueue => bonusQueue;//todo make readonly
+
+    //todo replace these
+    private string[] tempWeapons = new string[3] { "Shotgun", "Revolver", "Katana" };
+    private string[] tempTraps = new string[3] { "Barbed Wire", "Turret ", "Mine" };
+
+    private void OnValidate()
+    {
+        if (tempWeapons.Length <= 2)
+        {
+            Debug.LogError($"{nameof(tempWeapons)} must have at least 3 entries or its queue generation will never complete due to the 'no two of the same objects should be in the queue beside each other' rule.");
+        }
+
+        if (tempTraps.Length <= 2)
+        {
+            Debug.LogError($"{nameof(tempTraps)} must have at least 3 entries or its queue generation will never complete due to the 'no two of the same objects should be in the queue beside each other' rule.");
+        }
+
+        if (Enum.GetNames(typeof(Bonuses)).Length <= 2)
+        {
+            Debug.LogError($"{nameof(Bonuses)} must have at least 3 entries or its queue generation will never complete due to the 'no two of the same objects should be in the queue beside each other' rule.");
+        }
+
+        if (SECONDS_TO_COUNT_TO != 10)
+        {
+#pragma warning disable CS0162
+            Debug.LogWarning($"Be sure to change {nameof(SECONDS_TO_COUNT_TO)} back to 10!");
+#pragma warning restore CS0162
+        }
+    }
 
     private void Awake()
     {
@@ -49,8 +102,16 @@ public class GameManager : MonoBehaviour
             Debug.Log(nameof(StartGame), this);
         }
 
+        bonusesLength = Enum.GetNames(typeof(Bonuses)).Length;
+
         SetGameState(GameStates.Play);
         StartGameTimer();
+
+        GenerateWeaponQueue();
+        GenerateTrapQueue();
+        GenerateBonusQueue();
+
+        ShiftQueues();
     }
 
     public void EndGame()
@@ -71,7 +132,7 @@ public class GameManager : MonoBehaviour
             Debug.Log(nameof(StartGameTimer), this);
         }
 
-        gameTimer = Count(10);
+        gameTimer = Count(SECONDS_TO_COUNT_TO);
         StartCoroutine(gameTimer);
         OnTimerStarted?.Invoke();
     }
@@ -99,7 +160,7 @@ public class GameManager : MonoBehaviour
         gameTimer = null;
     }
 
-    private void SetGameState(GameStates gameState)
+    public void SetGameState(GameStates gameState)
     {
         if (verboseLogging)
         {
@@ -125,8 +186,110 @@ public class GameManager : MonoBehaviour
         }
 
         OnTenSecondsPassed?.Invoke();
+        ShiftQueues();
 
-        gameTimer = Count(10);
+        gameTimer = Count(SECONDS_TO_COUNT_TO);
         StartCoroutine(gameTimer);
+    }
+
+    private void ShiftQueues()
+    {
+        if (verboseLogging)
+        {
+            Debug.Log(nameof(ShiftQueues), this);
+        }
+
+        string newWeapon = weaponQueue.Dequeue();
+        OnNewWeapon?.Invoke(newWeapon);
+
+        string newTrap = trapQueue.Dequeue();
+        OnNewTrap?.Invoke(newTrap);
+
+        Bonuses bonus = bonusQueue.Dequeue();
+        OnNewBonus?.Invoke(bonus);
+
+        if (weaponQueue.Count <= repopulateQueueAtCount)
+        {
+            GenerateWeaponQueue();
+        }
+
+        if (trapQueue.Count <= repopulateQueueAtCount)
+        {
+            GenerateTrapQueue();
+        }
+
+        if (bonusQueue.Count <= repopulateQueueAtCount)
+        {
+            GenerateBonusQueue();
+        }
+    }
+
+    private void GenerateWeaponQueue()
+    {
+        if (verboseLogging)
+        {
+            Debug.Log(nameof(GenerateWeaponQueue), this);
+        }
+
+        while (weaponQueue.Count < queueMax)
+        {
+            int newWeaponIndex = UnityEngine.Random.Range(0, tempWeapons.Length);
+            string newWeapon = tempWeapons[newWeaponIndex];
+
+            while (newWeapon == lastQueuedWeapon)
+            {
+                newWeaponIndex = UnityEngine.Random.Range(0, tempWeapons.Length);
+                newWeapon = tempWeapons[newWeaponIndex];
+            }
+
+            weaponQueue.Enqueue(newWeapon);
+            lastQueuedWeapon = newWeapon;
+        }
+    }
+
+    private void GenerateTrapQueue()
+    {
+        if (verboseLogging)
+        {
+            Debug.Log(nameof(GenerateTrapQueue), this);
+        }
+
+        while (trapQueue.Count < queueMax)
+        {
+            int newTrapIndex = UnityEngine.Random.Range(0, tempTraps.Length);
+            string newTrap = tempTraps[newTrapIndex];
+
+            while (newTrap == lastQueuedTrap)
+            {
+                newTrapIndex = UnityEngine.Random.Range(0, tempTraps.Length);
+                newTrap = tempTraps[newTrapIndex];
+            }
+
+            trapQueue.Enqueue(newTrap);
+            lastQueuedTrap = newTrap;
+        }
+    }
+
+    private void GenerateBonusQueue()
+    {
+        if (verboseLogging)
+        {
+            Debug.Log(nameof(GenerateBonusQueue), this);
+        }
+
+        while (bonusQueue.Count < queueMax)
+        {
+            int newBonusIndex = UnityEngine.Random.Range(0, bonusesLength);
+            Bonuses newBonus = (Bonuses)newBonusIndex;
+
+            while (newBonus == lastQueuedBonus)
+            {
+                newBonusIndex = UnityEngine.Random.Range(0, bonusesLength);
+                newBonus = (Bonuses)newBonusIndex;
+            }
+
+            bonusQueue.Enqueue(newBonus);
+            lastQueuedBonus = newBonus;
+        }
     }
 }
--- Source/Assets/Scripts/Tests/GameManagerTester.cs
+++ Source/Assets/Scripts/Tests/GameManagerTester.cs
@@ -1,4 +1,3 @@
-using System;
 using TMPro;
 using UnityEngine;
 
@@ -8,6 +7,10 @@ public class GameManagerTester : MonoBehaviour
 
     [SerializeField] private TextMeshProUGUI secondText = null;
     [SerializeField] private GameObject tenSecondsPassedText = null;
+
+    [SerializeField] private TextMeshProUGUI weaponText = null;
+    [SerializeField] private TextMeshProUGUI trapText = null;
+    [SerializeField] private TextMeshProUGUI bonusText = null;
 
     [SerializeField] private GameObject startGameButton = null;
     [SerializeField] private GameObject endGameButton = null;
@@ -21,6 +24,10 @@ public class GameManagerTester : MonoBehaviour
         GameManager.Instance.OnSecondPassed += OnSecond;
         GameManager.Instance.OnTenSecondsPassed += OnTenSecondsPassed;
         GameManager.Instance.OnTimerStopped += OnTimerStopped;
+
+        GameManager.Instance.OnNewWeapon += OnNewWeapon;
+        GameManager.Instance.OnNewTrap += OnNewTrap;
+        GameManager.Instance.OnNewBonus += OnNewBonus;
     }
 
     private void OnGameStateChanged(GameStates gameState)
@@ -32,8 +39,17 @@ public class GameManagerTester : MonoBehaviour
 
         gameStateText.text = "Game State: " + gameState.ToString();
 
+        weaponText.gameObject.SetActive(gameState == GameStates.Play);
+        trapText.gameObject.SetActive(gameState == GameStates.Play);
+        bonusText.gameObject.SetActive(gameState == GameStates.Play);
+
         startGameButton.SetActive(gameState != GameStates.Play);
         endGameButton.SetActive(gameState == GameStates.Play);
+
+        if (gameState != GameStates.Play)
+        {
+            tenSecondsPassedText.SetActive(false);
+        }
     }
 
     private void OnSecond(int second)
@@ -69,5 +85,35 @@ public class GameManagerTester : MonoBehaviour
         }
 
         secondText.text = string.Empty;
+    }
+
+    private void OnNewWeapon(string weapon)
+    {
+        if (verboseLogging)
+        {
+            Debug.Log(nameof(OnNewWeapon) + " ( " + nameof(weapon) + ": " + weapon + " )", this);
+        }
+
+        weaponText.text = "Weapon: " + weapon;
+    }
+
+    private void OnNewTrap(string trap)
+    {
+        if (verboseLogging)
+        {
+            Debug.Log(nameof(OnNewTrap) + " ( " + nameof(trap) + ": " + trap + " )", this);
+        }
+
+        trapText.text = "Trap: " + trap;
+    }
+
+    private void OnNewBonus(Bonuses bonus)
+    {
+        if (verboseLogging)
+        {
+            Debug.Log(nameof(OnNewBonus) + " ( " + nameof(bonus) + ": " + bonus + " )", this);
+        }
+
+        bonusText.text = "Bonus: " + bonus;
     }
 }
