8daec5da1c93b9f69d2046a4a2c605deae20aeb3
MahadAdnanGul <64431074+MahadAdnanGul@users.noreply.github.com>
2022-10-03 02:58:55 +0500

    WIP item blueprints

--- Source/Assets/Scripts/Objects/TrapBlueprint.cs
+++ Source/Assets/Scripts/Objects/TrapBlueprint.cs
@@ -8,14 +8,18 @@ public class TrapBlueprint : Item
     [SerializeField] private float placementRange;
     [SerializeField] private LayerMask placementLayer;
     [SerializeField] private GameObject trapObj;
+    [SerializeField] private Material validMaterial;
+    [SerializeField] private Material invalidMaterial;
     private Camera mainCamera;
     private bool _isEquipped = false;
     private Mesh _objMesh;
+    private MeshRenderer _meshRenderer;
 
     private void Awake()
     {
         mainCamera=Camera.main;
         _objMesh = trapObj.GetComponent<MeshFilter>().mesh;
+        _meshRenderer = trapObj.GetComponent<MeshRenderer>();
     }
 
     void Start()
@@ -33,19 +37,42 @@ public class TrapBlueprint : Item
 
         if (_isEquipped)
         {
+            trapObj.transform.position = mainCamera.transform.position + mainCamera.transform.forward * 2f;//+new Vector3(0,_objMesh.bounds.extents.y,0);
             RaycastHit rayHit;
-            if (Physics.Raycast(mainCamera.transform.position, mainCamera.transform.forward, out rayHit, placementRange, placementLayer))
+            
+            Debug.DrawLine(trapObj.transform.position, trapObj.transform.position + Vector3.down * placementRange, Color.green, 1f);
+            if (Physics.BoxCast(trapObj.transform.position,_objMesh.bounds.extents,Vector3.down,out rayHit,transform.rotation,placementRange,placementLayer))
             {
-                if (rayHit.collider.gameObject.transform != null)
+                if (rayHit.collider.gameObject.layer == 3)
+                {
+                    trapObj.transform.position = new Vector3(rayHit.point.x,rayHit.point.y,rayHit.point.z);
+                    if (rayHit.collider.gameObject.transform != null)
+                    {
+                        _meshRenderer.material = validMaterial;
+                    }
+                    if (Input.GetKeyDown(KeyCode.C))
+                    {
+                        ConfirmBluePrintPlacement();
+                    }
+                }
+                else
                 {
                     trapObj.transform.position = new Vector3(rayHit.point.x,rayHit.point.y+_objMesh.bounds.extents.y,rayHit.point.z);
-                    trapObj.SetActive(true);
+                    if (rayHit.collider.gameObject.transform != null)
+                    {
+                        _meshRenderer.material = invalidMaterial;
+                    }
                 }
             }
             else
             {
-                trapObj.SetActive(false);
+                //_meshRenderer.material = invalidMaterial;
             }
+            trapObj.SetActive(true);
+        }
+        else
+        {
+            trapObj.SetActive(false);
         }
     }
 
@@ -56,6 +83,11 @@ public class TrapBlueprint : Item
 
     private void Equip()
     {
-        _isEquipped = true;
+        _isEquipped = !_isEquipped;
+    }
+
+    private void ConfirmBluePrintPlacement()
+    {
+        Debug.Log("Placement Confirmed");
     }
 }
