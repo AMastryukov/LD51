a3630cce2ea14f8783be50efe60886596ab92d6a
MahadAdnanGul <64431074+MahadAdnanGul@users.noreply.github.com>
2022-10-03 03:29:15 +0500

    Fixed some placement issues

--- Source/Assets/Scripts/Objects/TrapBlueprint.cs
+++ Source/Assets/Scripts/Objects/TrapBlueprint.cs
@@ -10,84 +10,83 @@ public class TrapBlueprint : Item
     [SerializeField] private GameObject trapObj;
     [SerializeField] private Material validMaterial;
     [SerializeField] private Material invalidMaterial;
-    private Camera mainCamera;
     private bool _isEquipped = false;
     private Mesh _objMesh;
     private MeshRenderer _meshRenderer;
+    private bool _onValidGround = false;
 
     private void Awake()
     {
-        mainCamera=Camera.main;
         _objMesh = trapObj.GetComponent<MeshFilter>().mesh;
         _meshRenderer = trapObj.GetComponent<MeshRenderer>();
-    }
 
-    void Start()
+    }
+    private void OnEnable()
     {
-        
+        GameManager.OnTenSecondsPassed += ExplicitUnEquip;
     }
 
-    // Update is called once per frame
+    private void OnDisable()
+    {
+        GameManager.OnTenSecondsPassed -= ExplicitUnEquip;
+    }
+    
     void Update()
     {
+        //Temporarily managing input here, should be hooked up to input handler
         if (Input.GetKeyDown(KeyCode.X))
         {
-            Equip();
+            Use();
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            ConfirmBluePrintPlacement();
         }
 
         if (_isEquipped)
         {
-            trapObj.transform.position = mainCamera.transform.position + mainCamera.transform.forward * 2f;//+new Vector3(0,_objMesh.bounds.extents.y,0);
             RaycastHit rayHit;
-            
-            Debug.DrawLine(trapObj.transform.position, trapObj.transform.position + Vector3.down * placementRange, Color.green, 1f);
-            if (Physics.BoxCast(trapObj.transform.position,_objMesh.bounds.extents,Vector3.down,out rayHit,transform.rotation,placementRange,placementLayer))
+            Debug.DrawLine(trapObj.transform.position+Vector3.up*placementRange/2, trapObj.transform.position + Vector3.down * placementRange, Color.green, 1f);
+            if (Physics.BoxCast(trapObj.transform.position+Vector3.up*placementRange/2,_objMesh.bounds.extents,Vector3.down,out rayHit,transform.rotation,placementRange,placementLayer))
             {
                 if (rayHit.collider.gameObject.layer == 3)
                 {
-                    trapObj.transform.position = new Vector3(rayHit.point.x,rayHit.point.y,rayHit.point.z);
-                    if (rayHit.collider.gameObject.transform != null)
-                    {
-                        _meshRenderer.material = validMaterial;
-                    }
-                    if (Input.GetKeyDown(KeyCode.C))
-                    {
-                        ConfirmBluePrintPlacement();
-                    }
+                    _onValidGround = true;
+                    _meshRenderer.material = validMaterial;
+                    
                 }
                 else
                 {
-                    trapObj.transform.position = new Vector3(rayHit.point.x,rayHit.point.y+_objMesh.bounds.extents.y,rayHit.point.z);
-                    if (rayHit.collider.gameObject.transform != null)
-                    {
-                        _meshRenderer.material = invalidMaterial;
-                    }
+                    _onValidGround = false;
+                    _meshRenderer.material = invalidMaterial;
                 }
-            }
-            else
-            {
-                //_meshRenderer.material = invalidMaterial;
             }
             trapObj.SetActive(true);
         }
         else
         {
+            _onValidGround = false;
             trapObj.SetActive(false);
         }
     }
 
     public override void Use()
     {
-        //Do smth
+        _isEquipped = !_isEquipped;
     }
 
-    private void Equip()
+    private void ExplicitUnEquip()
     {
-        _isEquipped = !_isEquipped;
+        _isEquipped = false;
     }
 
     private void ConfirmBluePrintPlacement()
     {
-        Debug.Log("Placement Confirmed");
+        if (!_isEquipped) return;
+        if (_onValidGround)
+        {
+            Debug.Log("Placement Confirmed");
+            ExplicitUnEquip();
+        }
     }
 }
