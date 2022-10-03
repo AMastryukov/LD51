49c8d9d3cac889efc14e6f7b222cab7209e64a5a
Alexander Mastryukov <Alexandermastryukov@gmail.com>
2022-10-03 19:02:54 -0400

    Add tutorial screen & music

---
+++ Source/Assets/Scripts/User Interface/TutorialScreen.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialScreen : MonoBehaviour
+{
+    [SerializeField] private Canvas[] tutorialPages;
+    [SerializeField] private bool showTutorial = true;
+
+    private CanvasGroup _canvasGroup;
+    private AudioSource _mainMenuTheme; // don't ask why it's here, no time
+
+    private void Awake()
+    {
+        _canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    private void Start()
+    {
+        if (showTutorial)
+        {
+            OpenTutorial();
+            return;
+        }
+
+        _mainMenuTheme.Play();
+    }
+
+    public void OpenTutorial()
+    {
+        Time.timeScale = 0f;
+
+        _canvasGroup.alpha = 1f;
+        _canvasGroup.interactable = true;
+        _canvasGroup.blocksRaycasts = true;
+
+        OpenPage(0);
+    }
+
+    public void CloseTutorial()
+    {
+        _canvasGroup.alpha = 0f;
+        _canvasGroup.interactable = false;
+        _canvasGroup.blocksRaycasts = false;
+
+        Time.timeScale = 1f;
+
+        _mainMenuTheme.Play();
+    }
+
+    public void OpenPage(int page)
+    {
+        if (page >= tutorialPages.Length) return;
+
+        CloseAll();
+        tutorialPages[page].enabled = true;
+    }
+
+    private void CloseAll()
+    {
+        foreach (var page in tutorialPages)
+        {
+            page.enabled = false;
+        }
+    }
+}
